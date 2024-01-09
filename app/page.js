9271aef85a8652bf38d895b3d2121755edfc13a1
faisalSha4536 <shabbirfaisal6732@gmail.com>
2024-01-09 03:00:01 -0800

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,17 @@
+'use client'
+import Tic from '@/Components/TicTac/Tic'
+import React, { useState } from 'react'
+
+
+const page = () => {
+
+
+
+  return (   
+    <div>
+      <Tic/>
+    </div>
+  )
+}
+
+export default page
